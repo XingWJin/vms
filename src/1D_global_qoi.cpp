dfcf11b480acc04d0c68fc4ca2265c4e680f6381
Brian Granzow <brian.granzow@gmail.com>
2016-09-07 08:15:37 -0400

    solve in first example

--- src/1D_global_qoi.cpp
+++ src/1D_global_qoi.cpp
@@ -4,8 +4,8 @@
 
 namespace {
 
-static const double k=1.0e-3;
-static const double a=1.0;
+double k;
+double a;
 
 double f(apf::Vector3 const& x) {
   return 1.0;
@@ -47,6 +47,8 @@ static void setup_input(vms::Input* in, char** argv) {
   in->k = atof(argv[2]);
   in->a[0] = atof(argv[3]);
   in->output_name = std::string(argv[4]);
+  k = in->k;
+  a = in->a[0];
   vms::print("running with the inputs:");
   vms::print(" num 1D grid elems: %d", in->num_elems);
   vms::print(" k:                 %e", in->k);
@@ -55,8 +57,9 @@ static void setup_input(vms::Input* in, char** argv) {
 }
 
 static void run_example(int argc, char** argv) {
-  vms::Mesh m(2,100,true);
-  m.write("output");
+  vms::Solver solver(&in);
+  solver.solve();
+  solver.get_mesh()->write(in.output_name.c_str());
 }
 
 }
